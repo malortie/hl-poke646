41ad1d032c5349fdcbf5e385ced3830a7e422a12
Marc-Antoine Lortie <marc-antoine.lortie@outlook.com>
2016-02-01 06:32:32 -0500

    Commit "Poke646 & Vendetta" source files.
    Fixed:
    
    - "Could not load library client." when launching the mod.
    -  liblist.gam now has correct parameters.

---
+++ poke646/cl_dll/tri.cpp
@@ -0,0 +1,74 @@
+//========= Copyright © 1996-2002, Valve LLC, All rights reserved. ============
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//=============================================================================
+
+// Triangle rendering, if any
+
+#include "hud.h"
+#include "cl_util.h"
+
+// Triangle rendering apis are in gEngfuncs.pTriAPI
+
+#include "const.h"
+#include "entity_state.h"
+#include "cl_entity.h"
+#include "triangleapi.h"
+#include "Exports.h"
+
+#include "particleman.h"
+#include "tri.h"
+extern IParticleMan *g_pParticleMan;
+
+/*
+=================
+HUD_DrawNormalTriangles
+
+Non-transparent triangles-- add them here
+=================
+*/
+void CL_DLLEXPORT HUD_DrawNormalTriangles( void )
+{
+//	RecClDrawNormalTriangles();
+
+	gHUD.m_Spectator.DrawOverview();
+}
+
+#if defined( _TFC )
+void RunEventList( void );
+#endif
+
+/*
+=================
+HUD_DrawTransparentTriangles
+
+Render any triangles with transparent rendermode needs here
+=================
+*/
+void CL_DLLEXPORT HUD_DrawTransparentTriangles( void )
+{
+//	RecClDrawTransparentTriangles();
+
+#if defined( _TFC )
+	RunEventList();
+#endif
+
+	if ( g_pParticleMan )
+		 g_pParticleMan->Update();
+}
+
+#if defined ( POKE646_CLIENT_DLL )
+/*
+=================
+HUD_DrawOrthoTriangles
+Orthogonal Triangles -- (relative to resolution,
+smackdab on the screen) add them here
+=================
+*/
+void HUD_DrawOrthoTriangles(void)
+{
+	gHUD.m_Scope.DrawScope();
+}
+#endif // defined ( POKE646_CLIENT_DLL )
