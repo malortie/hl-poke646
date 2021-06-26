12b9480e5a5fe960a31010b119d6ea23b683d6b3
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-06-26 11:25:16 -0400

    Use std::min.

--- dlls/poke646/robocop.cpp
+++ dlls/poke646/robocop.cpp
@@ -1372,7 +1372,7 @@ void CRoboCop::BeamUpdate(void)
 
 	if (m_pBeamSpot)
 	{
-		m_pBeamSpot->pev->renderamt = UTIL_Approach(min(m_beamBrightness + 25, 255), m_pBeamSpot->pev->renderamt, 60);
+		m_pBeamSpot->pev->renderamt = UTIL_Approach(std::min(m_beamBrightness + 25, 255), m_pBeamSpot->pev->renderamt, 60);
 		if (m_pBeamSpot->pev->renderamt == 0)
 			m_pBeamSpot->pev->effects |= EF_NODRAW;
 		else
