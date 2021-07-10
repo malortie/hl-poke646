081fc6e1c428ed02f567189aaaaa9497e878639a
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-07-10 11:41:00 -0400

    Reworked fire trail code.

--- dlls/poke646/firetrail.cpp
+++ dlls/poke646/firetrail.cpp
@@ -25,21 +25,21 @@ class CFireTrail : public CBaseEntity
 {
 	void Spawn(void);
 	void Think(void);
-	void Touch(CBaseEntity *pOther);
-	int ObjectCaps(void) { return FCAP_DONT_SAVE; }
 };
 
 LINK_ENTITY_TO_CLASS(fire_trail, CFireTrail);
 
 void CFireTrail::Spawn(void)
 {
-	pev->velocity = RANDOM_FLOAT(200, 300) * pev->angles;
-	pev->velocity.x += RANDOM_FLOAT(-100.f, 100.f);
-	pev->velocity.y += RANDOM_FLOAT(-100.f, 100.f);
+	pev->velocity = pev->angles; // vecPlaneNormal is stored in pev->angles.
+	pev->velocity.x += RANDOM_FLOAT(-1, 1);
+	pev->velocity.y += RANDOM_FLOAT(-1, 1);
 	if (pev->velocity.z >= 0)
-		pev->velocity.z += 200;
+		pev->velocity.z += RANDOM_FLOAT(0.5f, 1.0f);
 	else
-		pev->velocity.z -= 200;
+		pev->velocity.z -= RANDOM_FLOAT(0.5f, 1.0f);
+	pev->velocity = pev->velocity * RANDOM_FLOAT(200, 300);
+
 	pev->movetype = MOVETYPE_BOUNCE;
 	pev->gravity = 0.5;
 	pev->nextthink = gpGlobals->time + 0.1;
@@ -47,37 +47,26 @@ void CFireTrail::Spawn(void)
 	SET_MODEL(edict(), "models/grenade.mdl");	// Need a model, just use the grenade, we don't draw it anyway
 	UTIL_SetSize(pev, g_vecZero, g_vecZero);
 	pev->effects |= EF_NODRAW;
-	pev->speed = RANDOM_FLOAT(0.5, 1.5);
-	pev->maxspeed = pev->speed;
+	pev->speed = RANDOM_FLOAT(0.5, 1.5); // Start scale of the fire trail.
 
 	pev->angles = g_vecZero;
 }
 
-
 void CFireTrail::Think(void)
 {
-	CSprite* pSprite = CSprite::SpriteCreate("sprites/zerogxplode.spr", pev->origin, TRUE);
-	pSprite->AnimateAndDie(RANDOM_FLOAT(15.0f, 20.0f));
+	CSprite* pSprite = CSprite::SpriteCreate("sprites/zerogxplode.spr", pev->origin, FALSE);
+	pSprite->pev->spawnflags |= SF_SPRITE_ONCE; // Play sprite only once.
+	pSprite->pev->framerate = RANDOM_FLOAT(10, 15);
+	pSprite->TurnOn();
+	pSprite->AnimateAndDie(pSprite->pev->framerate); // Kill the sprite after it is done playing.
 	pSprite->SetTransparency( kRenderTransAdd, 255, 255, 255, 192, kRenderFxNoDissipation );
 	pSprite->SetScale(pev->speed);
-	pSprite->pev->frame = pSprite->Frames() - ((std::max(0.0f, pev->speed - (0.1f * pSprite->pev->framerate)) * pSprite->Frames()) / pev->maxspeed);
 
-	pev->speed -= 0.1;
+	pev->speed -= 0.1; // Make smaller fire trail next think next think.
 	if (pev->speed > 0)
 		pev->nextthink = gpGlobals->time + 0.1;
 	else
 		UTIL_Remove(this);
 
 	pev->flags &= ~FL_ONGROUND;
-}
-
-void CFireTrail::Touch(CBaseEntity *pOther)
-{
-	if (pev->flags & FL_ONGROUND)
-		pev->velocity = pev->velocity * 0.1;
-	else
-		pev->velocity = pev->velocity * 0.6;
-
-	if ((pev->velocity.x*pev->velocity.x + pev->velocity.y*pev->velocity.y) < 10.0)
-		pev->speed = 0;
 }
