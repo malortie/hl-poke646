274140727fe603b940be355cb18d21655b3142da
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-07-31 12:54:29 -0400

    Fixed nail emitting sparks when touching sky surface.

--- dlls/poke646/nail.cpp
+++ dlls/poke646/nail.cpp
@@ -120,7 +120,8 @@ void CNail::NailTouch(CBaseEntity *pOther)
 			pev->nextthink = gpGlobals->time + 2.0;
 		}
 
-		if (UTIL_PointContents(pev->origin) != CONTENTS_WATER && RANDOM_LONG(0,5) == 0)
+		int contents = UTIL_PointContents(pev->origin);
+		if (contents != CONTENTS_WATER && contents != CONTENTS_SKY && RANDOM_LONG(0,5) == 0)
 		{
 			UTIL_Sparks(pev->origin);
 		}
