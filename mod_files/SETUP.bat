d5884f3e307c0ef2d46b4927bf104919354f4d0f
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2021-11-06 16:23:32 -0400

    Adapt mod setup script.

--- mod_files/SETUP.bat
+++ mod_files/SETUP.bat
@@ -3,6 +3,16 @@
 @echo off
 
 echo Setup starting...
+
+:: Copy mp3 sound files to media folder.
+xcopy /f /i /y /s sound\mp3\*.mp3 media\mp3\
+
+:: Disable the use of delta.lst
+if exist delta.lst (
+  echo Renaming delta.lst to delta.old.lst ...
+  ren delta.lst delta.old.lst
+)
+
 echo Setup successfully completed
 
 endlocal
